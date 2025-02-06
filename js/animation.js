78d7380467ef5b9203f36d6474091908e4250718
studentdev-glitch <monetary000@outlook.com>
2025-02-06 17:46:00 +0300

    fixed animation design

--- js/animation.js
+++ js/animation.js
@@ -5,9 +5,3 @@ gsap.to(".background", {
     repeat: -1,
     yoyo: true
 });
-
-document.addEventListener("mousemove", (e) => {
-    let x = (e.clientX / window.innerWidth - 0.5) * 10;
-    let y = (e.clientY / window.innerHeight - 0.5) * 10;
-    gsap.to(".background", { x: x, y: y, duration: 0.5 });
-});
